154a9d83f04e1fec32db98775739c05fb68b525e
Bruno Calou <brunocaloualves@gmail.com>
2023-10-13 15:01:46 -0300

    feat: add first page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      accent: "#4E9590",
+      invalid: "#BF0E0E",
+      brackets: "#F2CAB8",
+      "gray-50": "#F7F7F7",
+      "gray-100": "#E4E4E4",
+      "gray-200": "#BFBFBF",
+      black: "#000000",
+      white: "#FFFFFF",
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Inter", sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
