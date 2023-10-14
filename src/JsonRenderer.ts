708e3bba1796dd1127897f71bf6f65ba809b7f0a
Bruno Calou <brunocaloualves@gmail.com>
2023-10-14 15:22:14 -0300

    feat: delegate vertical lines render to the lines

--- src/JsonRenderer.ts
+++ src/JsonRenderer.ts
@@ -3,6 +3,7 @@ import { Virtualizer } from "./Virtualizer";
 export type LineParent = "object" | "array";
 
 export class JsonRenderer {
+  static tabSize = 16;
   private json: Record<string, any>;
   private root: HTMLElement;
   private virtualizer: Virtualizer;
@@ -58,24 +59,24 @@ export class JsonRenderer {
     return wrapper;
   }
 
-  private static countLines(json: Record<string, any>): number {
-    let counter = 0;
+  //   private static countLines(json: Record<string, any>): number {
+  //     let counter = 0;
 
-    for (let value of Object.values(json)) {
-      const isArray = Array.isArray(value);
-      const isObject = value instanceof Object;
+  //     for (let value of Object.values(json)) {
+  //       const isArray = Array.isArray(value);
+  //       const isObject = value instanceof Object;
 
-      if (isArray) {
-        counter += 2 + JsonRenderer.countLines(value);
-      } else if (isObject) {
-        counter += 1 + JsonRenderer.countLines(value);
-      } else {
-        counter += 1;
-      }
-    }
+  //       if (isArray) {
+  //         counter += 2 + JsonRenderer.countLines(value);
+  //       } else if (isObject) {
+  //         counter += 1 + JsonRenderer.countLines(value);
+  //       } else {
+  //         counter += 1;
+  //       }
+  //     }
 
-    return counter;
-  }
+  //     return counter;
+  //   }
 
   private static toLines(
     json: Record<string, any>,
@@ -89,20 +90,11 @@ export class JsonRenderer {
       const isObject = value instanceof Object;
 
       if (isArray) {
-        lines.push(
-          JsonRenderer.getArrayLine(key, JsonRenderer.countLines(value), depth),
-        );
+        lines.push(JsonRenderer.getArrayLine(key, depth));
         lines.push(...JsonRenderer.toLines(value, depth + 1, "array"));
         lines.push(JsonRenderer.getCloseArrayLine(depth));
       } else if (isObject) {
-        lines.push(
-          JsonRenderer.getObjectLine(
-            key,
-            JsonRenderer.countLines(value),
-            depth,
-            parent,
-          ),
-        );
+        lines.push(JsonRenderer.getObjectLine(key, depth, parent));
         lines.push(...JsonRenderer.toLines(value, depth + 1, "object"));
       } else {
         lines.push(JsonRenderer.getPropertyLine(key, value, depth, parent));
@@ -112,14 +104,10 @@ export class JsonRenderer {
     return lines;
   }
 
-  private static getArrayLine(
-    value: string,
-    amountOfLinesInside: number,
-    depth: number,
-  ): HTMLElement {
+  private static getArrayLine(value: string, depth: number): HTMLElement {
     const div = document.createElement("div");
     div.classList.add("relative");
-    div.style.paddingLeft = `${16 * depth}px`;
+    div.style.marginLeft = `${JsonRenderer.tabSize * depth}px`;
 
     const span1 = document.createElement("span");
     span1.classList.add("text-accent");
@@ -131,23 +119,24 @@ export class JsonRenderer {
     span2.appendChild(document.createTextNode("["));
     div.appendChild(span2);
 
-    div.appendChild(JsonRenderer.getVerticalLine(amountOfLinesInside));
+    JsonRenderer.appendVerticalLines(div, depth);
 
     return div;
   }
 
   private static getCloseArrayLine(depth = 0): HTMLElement {
     const div = document.createElement("div");
-    div.classList.add("text-brackets");
-    div.style.paddingLeft = `${16 * depth}px`;
+    div.classList.add("text-brackets", "relative");
+    div.style.marginLeft = `${JsonRenderer.tabSize * depth}px`;
     div.appendChild(document.createTextNode("]"));
+
+    JsonRenderer.appendVerticalLines(div, depth);
 
     return div;
   }
 
   private static getObjectLine(
     index: string,
-    amountOfLinesInside: number,
     depth: number,
     lineParent: LineParent,
   ): HTMLElement {
@@ -156,32 +145,29 @@ export class JsonRenderer {
       "relative",
       lineParent === "object" ? "text-accent" : "text-gray-200",
     );
-    div.style.marginLeft = `${16 * depth}px`;
+    div.style.marginLeft = `${JsonRenderer.tabSize * depth}px`;
 
     const span1 = document.createElement("span");
     span1.appendChild(document.createTextNode(`${index}:`));
     div.appendChild(span1);
 
-    div.appendChild(JsonRenderer.getVerticalLine(amountOfLinesInside));
+    JsonRenderer.appendVerticalLines(div, depth);
 
     return div;
   }
 
-  private static getVerticalLine(amountOfLines: number) {
+  private static getVerticalLine(depth: number) {
     const verticalLine = document.createElement("span");
 
     verticalLine.classList.add(
       "absolute",
-      "left-0",
       "h-full",
       "w-[1px]",
       "origin-top-left",
       "border-l-[1px]",
       "border-gray-200",
     );
-    verticalLine.style.transform = `translateY(100%) scaleY(${
-      amountOfLines * 100
-    }%)`;
+    verticalLine.style.left = `${-JsonRenderer.tabSize * (depth + 1)}px`;
 
     return verticalLine;
   }
@@ -196,7 +182,7 @@ export class JsonRenderer {
 
     const div = document.createElement("div");
     div.classList.add("relative");
-    div.style.marginLeft = `${16 * depth}px`;
+    div.style.marginLeft = `${JsonRenderer.tabSize * depth}px`;
 
     const span1 = document.createElement("span");
     span1.classList.add(
@@ -206,6 +192,14 @@ export class JsonRenderer {
     div.appendChild(span1);
     div.appendChild(document.createTextNode(formattedValue));
 
+    JsonRenderer.appendVerticalLines(div, depth);
+
     return div;
+  }
+
+  private static appendVerticalLines(target: HTMLElement, depth: number) {
+    for (let i = 0; i < depth; i += 1) {
+      target.appendChild(JsonRenderer.getVerticalLine(i));
+    }
   }
 }
